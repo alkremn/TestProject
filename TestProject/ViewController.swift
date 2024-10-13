e0b1b39d3f750cbfd54e0eabf7986802381cfaa2
Alexey Kremnev <alkremn@gmail.com>
2024-10-12 22:45:19 -0600

    Initial Commit

---
+++ TestProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TestProject
+//
+//  Created by Alexey Kremnev on 10/7/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
