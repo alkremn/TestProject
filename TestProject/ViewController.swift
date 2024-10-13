5da42b9eedcd70613a6996ea6d4773e5ee981b94
Alexey Kremnev <alkremn@gmail.com>
2024-10-12 23:16:16 -0600

    Removed unused print statement and comment

--- TestProject/ViewController.swift
+++ TestProject/ViewController.swift
@@ -11,12 +11,12 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
 
     
     @IBAction private func buttonDidTap(_ sender: Any) {
-        print("button tapped")
+        
     }
 
 }
